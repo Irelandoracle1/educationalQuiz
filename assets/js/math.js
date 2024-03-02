d88cb545856b1edd3074f1152efdb0b5a6209de9
Irelandoracle1 <nairajnr1@gmail.com>
2024-03-02 10:20:57 +0000

    created a quiz display function, made sure the questions and answers are displayed with a loop,avoiding using var to create variables, also pushed the questions into an empty array, when quiz result button is clicked , added function dispay result to show answer, also used try and catch to catch errors

--- assets/js/math.js
+++ assets/js/math.js
@@ -69,3 +69,76 @@ let mathQuestions = [
 let  scoreContainer=document.getElementById('score');
 let quizContainer=document.getElementById('quiz');
 let  submitButton=document.getElementById('submit');
+
+//step three: create  a quiz display function
+
+//this function diplays questions and answers
+//from a loop
+//step three: create  a quiz display function
+
+//this function diplays questions and answers
+//from a loop
+function displayQuestions(questions, quizcontainer){
+    var displayOutput=[];
+    var mathAnswers;
+  try{
+  for(var i=0; i<=questions.length; i++){
+      mathAnswers=[];
+  
+    for(var letter in questions[i].answer){
+      if(questions[i].answer !==null){
+          mathAnswers.push(
+              '<div>'+
+            '<input type="radio" class="radio-button" name="question'+i+'" value="'+letter+'">'+'<span>' +letter+" : "+questions[i].answer[letter] +'</span>'+'</div>');
+          
+      }
+      } //this the end of the answers loop
+       
+      displayOutput.push(
+          '<div class="question">'+questions[i].question +'</div>'+'<div class="answers">'+mathAnswers.join('')+'</div>'
+      );
+      quizcontainer.innerHTML=displayOutput.join('');
+  } //end of main for loop
+  }catch(e){}
+  }
+  // this function helps us to display answers
+  //once the get quiz result button is clicked
+  function displayResults(questions, quizcontainer, scorecontainer){
+    //grab all the answers div
+   var userAnswerContainers=quizcontainer.querySelectorAll('.answers');
+   
+   //track users answers
+   var userAnswer="";
+   //initilize the right answers
+   var numberOfCorrectAnswers=0;
+  
+   for(var v=0; v<=questions.length - 1; v++){
+      
+    userAnswer=(userAnswerContainers[v].querySelector('input[name=question'+v+']:checked')  || {}).value;
+   if(userAnswer===questions[v].rightAnswer){
+      numberOfCorrectAnswers++;
+       //add correct answer image
+       userAnswerContainers[v].innerHTML="<img src='assets/images/img/right.jpg'>";
+  
+   }else if(userAnswer==null || userAnswer=="undefined"){
+      userAnswerContainers[v].innerHTML="<i style='color:orange;'>Choose an answer</i>";
+      }
+   else{
+      userAnswerContainers[v].innerHTML="<img src='assets/images/img/wrong.png'>";
+   }
+  
+   }
+   scorecontainer.innerHTML=numberOfCorrectAnswers + "/" +questions.length;
+  
+   }
+  
+  
+  
+  //call to the dsiplay question function
+  
+  displayQuestions(mathQuestions, quizContainer);
+  
+  submitButton.onclick=function(){
+  
+      displayResults(mathQuestions, quizContainer, scoreContainer);
+  };
